81cc6d706ee730249edbf8d8ca35d77ee4811644
unknown <leandro.rochasouza98@gmail.com>
2024-01-11 09:59:20 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="description" content="Um site comum qualquer">
+	<meta charset="keywords" content="Site Comum, Site Teste, Teste">
+	<link rel="stylesheet" href="css/estile.css">
+	<link rel="stylesheet" href="css/responsive.css">
+	<title> Site Teste </title>
+</head>
+<body>
+	<nav class="navbar">
+		<div class="logo">
+			<h1> Site Teste </h1>
+		</div>
+
+		<div class="menu">
+			<a href="#"> Home </a>
+			<a href="#"> Sobre Nós </a>
+			<a href="#"> Serviços </a>
+			<a id="botao" href="#"> Fale Conosco </a>
+		</div>
+	</nav>
+
+	<header class="header">
+		<div class="headline">
+			<h2 style="letter-spacing: 10px;"> Você achou o melhor </h2>
+			<h2 style="font-size: 80px;"> Site Teste </h2>
+			<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+			exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+			ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. </p>
+			<a href="#" class="btn-contact"> Contrate Agora </a>
+		</div>
+		<div class="img-headline">
+			<img style="max-width: 90%;" src="img/hero_right.png">
+		</div>
+	</header>	
+
+	<section>
+		<h2> O que podemos fazer pelo seu negócio? </h2>
+		<div class="servicos">
+			<div class="card">
+				<img src="img/pc.png">
+				<div class="card-text"> 
+					<h3> Desenvolvimento de Sites </h3>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+					exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+					ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. </p>
+				</div>
+				<div class="texto-oculto">
+					<h3> Outro Texto Qualquer </h3>
+					<p> 
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+					exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+					ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. 
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+					exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+					ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. 
+					</p>
+				</div>
+			</div>
+			<div class="card">
+			<img src="img/smartphone.png">
+				<div class="card-text">
+					<h3> Desenvolvimento de Apps </h3>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+					exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+					ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. </p>
+				</div>
+				<div class="texto-oculto">
+					<h3> Outro Texto Qualquer </h3>
+					<p> 
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+					exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+					ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. 
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+					exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+					ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. 
+					</p>
+				</div>
+			</div>
+			<div class="card">
+			<img src="img/marketing.png">
+				<div class="card-text">						
+					<h3> Marketing Digital </h3>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+					exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+					ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. </p>
+				</div>
+				<div class="texto-oculto">
+					<h3> Outro Texto Qualquer </h3>
+					<p> 
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+					exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+					ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. 
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis 
+					exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet 
+					ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas. 
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<div class="logo-rodape">
+			<h1> Site Teste </h1>
+			<p> Um site qualquer desenvolvido com HTML e CSS </p>
+		</div>
+			<b> © Todos os direitos reservados. </b>
+	</footer>
+
+	<a href="#"> <img class="whats" src="img/whats.png"> </a>
+</body>
+</html>
